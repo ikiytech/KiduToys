0f062c69cbec11a803952b846b19eb4b2c8539fd
Muhammad Rifki <60310528+ikiytech@users.noreply.github.com>
2025-01-19 18:45:50 +0700

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'sql203.infinityfree.com';  // Host database
-$user = 'if0_37983334';            // MySQL Username
-$pass = '1WnBEape3lD9h';           // MySQL Password
-$dbname = 'if0_37983334_db_toko_mainan';      // MySQL Database Name
+$user = '';            // MySQL Username
+$pass = '';           // MySQL Password
+$dbname = '';      // MySQL Database Name
 
 // Buat koneksi ke database
 $conn = new mysqli($host, $user, $pass, $dbname);
